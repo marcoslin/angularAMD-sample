222ab1cea7912a24dfd2cda05f89fe919dee6534
Marcos Lin <linm.linmair@ebacimail.com>
2014-10-11 18:07:50 +0200

    Fixed the min problem and added detection of sub-states

--- app/scripts/main/nav_service.js
+++ app/scripts/main/nav_service.js
@@ -1,12 +1,20 @@
 define(['angularAMD'], function (angularAMD) {
   'use strict';
-  angularAMD.controller('navMenuController', function ($scope, $state) {
+  angularAMD.controller('navMenuController', ['$scope', '$state', function ($scope, $state) {
     $scope.isTabActive = function (tabName) {
-      if (tabName === $state.current.name) {
+      // Check if there is sub-states
+      var stateName = $state.current.name,
+        subStatePos = stateName.indexOf('.');
+
+      if (subStatePos > -1) {
+        stateName = stateName.substring(0,subStatePos);
+      }
+
+      if (tabName === stateName) {
         return 'active';
       }
     };
-  });
+  }]);
 
   angularAMD.directive('navMenu', function () {
     return {
